6d9e39eab7666674a2eb4349743eabe8dc798881
Aniruddha Adhikary <932949+aniruddha-adhikary@users.noreply.github.com>
2024-12-10 10:01:09 +0800

    fix: properly handle the params promise

--- src/app/inbox/[id]/page.tsx
+++ src/app/inbox/[id]/page.tsx
@@ -1,18 +1,18 @@
 import { db } from "@/lib/db";
 
-export default async function InboxPage({ params }: { params: { id: string } }) {
-  const { id } = params;
-  const messages = await db.getInboxMessages(id);
+export default async function InboxPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const messages = await db.getInboxMessages(params.id);
 
   return (
     <div>
-      <h1>Inbox: {id}</h1>
+      <h1>Inbox: {params.id}</h1>
       <ul>
         {messages.map((message, index) => (
           <li key={index}>{message}</li>
         ))}
       </ul>
-      <a href={`/send/${id}`}>Send a Message</a>
+      <a href={`/send/${params.id}`}>Send a Message</a>
     </div>
   );
 }
